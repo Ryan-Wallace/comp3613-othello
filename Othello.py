c506aff6583a92e6667dc9a95db4a8c7c83b618b
Ryan Wallace <RyanLWallace@protonmail.com>
2021-11-30 15:45:45 -0400

    first commit

---
+++ Othello.py
@@ -0,0 +1,22 @@
+
+class OthhelloGame:
+	empty = '-'
+	black = 'o'
+	white = 'x'
+	border = '='
+
+	def __init__(self):
+		self.gameBoard  
+
+	def gameBoard():
+		board = [[]]*10
+		print(board)n
+
+
+def setPiece(board, x, y, piece):
+	board[x][y] = piece
+
+
+
+
+ 
